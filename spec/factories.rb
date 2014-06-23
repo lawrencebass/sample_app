071a365ebb12e16dd3b923855facd5ace6378df4
Lawrence Bass <lbass01@rams.shepherd.edu>
2014-06-23 03:01:44 -0400

    Finish user signup

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name     "Lawrence Bass"
+    email    "lbass01@rams.shepherd.edu"
+    password "lawrence1992"
+    password_confirmation "lawrence1992"
+  end
+end
